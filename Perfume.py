2663a901fb974ef55b4b32b46ac3fb006c27661c
mdodovic <matija.dodovic@gmail.com>
2021-12-27 11:27:44 +0100

    create Perfume entity

---
+++ Perfume.py
@@ -0,0 +1,19 @@
+class Perfume:
+
+    def __init__(self, brand, description, perfume_type, sex, volume_type, volume_set, volume_amount, price, parent_file):
+        self.brand = brand
+        self.description = description
+        self.perfume_type = perfume_type
+        self.sex = sex
+        self.volume_type = volume_type
+        self.volume_set = volume_set
+        self.volume_amount = volume_amount
+        self.price = price
+        self.parent_file = parent_file
+
+    def get_description(self):
+        return self.brand, self.description, self.perfume_type, self.sex, self.volume_type, self.volume_set, \
+               self.volume_amount
+
+    def get_price(self):
+        return self.price
---
+++ PerfumeMap.py
@@ -0,0 +1,8 @@
+import Perfume
+
+
+class PerfumeMap:
+
+    def __init__(self):
+        self.perfume_map = {}
+
