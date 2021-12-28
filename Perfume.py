59385288ec2e647099c582f7dddda8617c3dac57
mdodovic <matija.dodovic@gmail.com>
2021-12-28 12:29:10 +0100

    override __str__

--- Perfume.py
+++ Perfume.py
@@ -1,6 +1,7 @@
 class Perfume:
 
-    def __init__(self, brand, description, perfume_type, sex, volume_type, volume_set, volume_amount, price, parent_file):
+    def __init__(self, brand, description, perfume_type, sex, volume_type, volume_set, volume_amount, price,
+                 parent_file):
         self.brand = brand
         self.description = description
         self.perfume_type = perfume_type
@@ -17,3 +18,6 @@ class Perfume:
 
     def get_price(self):
         return self.price
+
+    def __str__(self) -> str:
+        return f"Perfume[{self.parent_file}] ({self.brand}, {self.description}, {self.perfume_type}, {self.sex}, {self.volume_type}, {self.volume_set}, {self.volume_amount}) -> {self.price}"
