aace54587624dbb67af11aa38127889de28a9236
mdodovic <matija.dodovic@gmail.com>
2021-12-28 13:15:14 +0100

    add volume metadata

--- Perfume.py
+++ Perfume.py
@@ -1,23 +1,25 @@
 class Perfume:
 
-    def __init__(self, brand, description, perfume_type, sex, volume_type, volume_set, volume_amount, price,
-                 parent_file):
+    def __init__(self, brand, description, perfume_type, sex, volume_tester, volume_set, volume_amount, volume_metadata,
+                 price, parent_file):
         self.brand = brand
         self.description = description
         self.perfume_type = perfume_type
         self.sex = sex
-        self.volume_type = volume_type
+        self.volume_tester = volume_tester
         self.volume_set = volume_set
         self.volume_amount = volume_amount
+        self.volume_metadata = volume_metadata
         self.price = price
         self.parent_file = parent_file
 
     def get_description(self):
-        return self.brand, self.description, self.perfume_type, self.sex, self.volume_type, self.volume_set, \
+        return self.brand, self.description, self.perfume_type, self.sex, self.volume_tester, self.volume_set, \
                self.volume_amount
 
     def get_price(self):
         return self.price
 
     def __str__(self) -> str:
-        return f"Perfume[{self.parent_file}] ({self.brand}, {self.description}, {self.perfume_type}, {self.sex}, {self.volume_type}, {self.volume_set}, {self.volume_amount}) -> {self.price}"
+        return f"Perfume[{self.parent_file}] ({self.brand}, {self.description}, {self.perfume_type}, {self.sex}, \
+                {self.volume_tester}, {self.volume_set}, {self.volume_amount}, {self.volume_metadata}) -> {self.price}"
