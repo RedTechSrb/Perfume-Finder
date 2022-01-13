09b9b0af1366ac9eedfb9d382297b6314c1a657f
mdodovic <matija.dodovic@gmail.com>
2022-01-13 14:07:53 +0100

    add map output, complete 2.excl

--- PerfumeMap.py
+++ PerfumeMap.py
@@ -6,3 +6,24 @@ class PerfumeMap:
     def __init__(self):
         self.perfume_map = {}
 
+    def insert_perfume(self, perfume: Perfume):
+        key = perfume.get_description()
+
+        if key in self.perfume_map.keys():
+            self.perfume_map[key].append((perfume.get_parent_file(), perfume.get_price()))
+        else:
+            self.perfume_map[key] = [(perfume.get_parent_file(), perfume.get_price())]
+
+    def __str__(self) -> str:
+
+        map_string = ""
+
+        for key, value in self.perfume_map.items():
+            map_string += str(key)
+            map_string += " -> "
+            map_string += str(value)
+            map_string += "\n"
+
+        return map_string
+
+
