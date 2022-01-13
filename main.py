5794dc169838f6aae5a1664e8c05e7d4cc2080d5
mdodovic <matija.dodovic@gmail.com>
2022-01-13 18:48:16 +0100

    complete 7.xlsx; merge all into one map

--- main.py
+++ main.py
@@ -233,20 +233,43 @@ def process_file_6(perfume_map, excel_file):
         perfume_map.insert_perfume(p)
 
 
+def make_ml_lowercase(volume):
+    if pd.isna(volume):
+        return None
+    return volume[0:-2] + volume[-2:].lower()
+
+
+def process_group_file_7(group):
+    volume_set = group == 'set'
+    volume_tester = group == 'TESTER'
+
+    volume_metadata = []
+    if volume_set == False and volume_tester == False:
+        volume_metadata = [group]
+
+    return {"volume_set": volume_set, "volume_tester": volume_tester, "volume_metadata": volume_metadata}
+
+
 def process_file_7(perfume_map, excel_file):
     data = pd.read_excel(excel_file)
 
+    data = data.dropna(subset=["PIRCE"])
     # BRANDS -> Brand
     # Naziv -> Description, volume_amount, sex
 
     perfume_list = [
-        (perfume.BRANDS,
-        perfume._3)
+        Perfume(perfume.BRANDS, perfume._3, perfume.TYPE,
+                "U",
+                process_group_file_7(perfume.GROUP)["volume_tester"],
+                process_group_file_7(perfume.GROUP)["volume_set"],
+                make_ml_lowercase(perfume.SIZE),
+                process_group_file_7(perfume.GROUP)["volume_metadata"],
+                perfume.PIRCE, 7)
         for perfume in data.itertuples()
     ]
     for p in perfume_list:
-        #perfume_map.insert_perfume(p)
-        print(p)
+        perfume_map.insert_perfume(p)
+        # print(p)
 
 
 def main():
@@ -256,7 +279,7 @@ def main():
     perfume_map = PerfumeMap()
     for root, dirs, files in os.walk(root_folder):
         for filename in files:
-            """
+
             if filename == "2.xlsx":
                 process_file_2(perfume_map, os.path.join(root, filename))
             elif filename == "3.xlsx":
@@ -267,12 +290,12 @@ def main():
                 process_file_5(perfume_map, os.path.join(root, filename))
             elif filename == "6.xls":
                 process_file_6(perfume_map, os.path.join(root, filename))
-
             elif filename == "7.xlsx":
                 process_file_7(perfume_map, os.path.join(root, filename))
-            """
-            if filename == "7.xlsx":
-                process_file_7(perfume_map, os.path.join(root, filename))
+
+            #if filename == "7.xlsx":
+            #    process_file_7(perfume_map, os.path.join(root, filename))
+
     print(len(perfume_map.get_map()))
     print(perfume_map)
     """
