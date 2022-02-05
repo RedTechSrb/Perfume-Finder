5d6c7d602bd601e2463217189a1909fad37a1abd
mdodovic <matija.dodovic@gmail.com>
2022-02-05 15:26:57 +0100

    create uniform gender

---
+++ utility.py
@@ -0,0 +1,23 @@
+import pandas as pd
+
+
+def make_sex_uniform(sex):
+    if pd.isna(sex):
+        return None
+    sex = sex.upper()
+    if sex == "MEN":
+        return "M"
+    elif sex == "MAN":
+        return "M"
+    elif sex == "MUŠKI":
+        return "M"
+    elif sex == "WOMEN":
+        return "W"
+    elif sex == "WOMAN":
+        return "W"
+    elif sex == "ŽENSKI":
+        return "W"
+    elif sex == "UNISEX":
+        return "U"
+    else:
+        return "U"
