030884dcbc5da70c1264038d7d91247e0f128a0a
mdodovic <matija.dodovic@gmail.com>
2022-08-12 15:46:06 +0200

    Distinct tester and non-tester perfumes in V2.xls

--- process_file_V2.py
+++ process_file_V2.py
@@ -4,13 +4,19 @@ from PerfumeMap import PerfumeMap
 from utility import *
 
 
-def process_naziv_file_6(column_Naziv_data):
+def process_naziv_file_6(column_Naziv_data, column_Tip_data):
     if pd.isna(column_Naziv_data):
         return {"description": 'NAN', "sex": 'NAN', "volume_amount": 'NAN'}
 
     naziv_part_lists = column_Naziv_data.split(" / ")
 
-    return {"description": naziv_part_lists[0], "sex": naziv_part_lists[2], "volume_amount": naziv_part_lists[1]}
+    volume_tester = column_Tip_data == 'Tester'
+    Description = naziv_part_lists[0]
+    if 'TESTER' not in Description:
+        if volume_tester:
+            Description = Description + ' tester'
+
+    return {"description": Description, "sex": naziv_part_lists[2], "volume_amount": naziv_part_lists[1]}
 
 
 def process_tip_file_6(column_Tip_data):
@@ -25,9 +31,9 @@ def process_tip_file_6(column_Tip_data):
     return {"volume_set": volume_set, "volume_tester": volume_tester, "volume_metadata": volume_metadata}
 
 
-def process_file_6(perfume_map: PerfumeMap, excel_file):
+def process_file_V2(perfume_map: PerfumeMap, excel_file):
 
-    print("File 6.xlsx processing ... ")
+    print("File V2.xls processing ... ")
 
     data = pd.read_excel(excel_file)
 
@@ -38,12 +44,12 @@ def process_file_6(perfume_map: PerfumeMap, excel_file):
 
     perfume_list = [
         Perfume(str(perfume._1),
-                str(process_naziv_file_6(perfume.Naziv)["description"]),
+                str(process_naziv_file_6(perfume.Naziv, perfume.Tip)["description"]),
                 str('NAN'),
-                make_sex_uniform(process_naziv_file_6(perfume.Naziv)["sex"]),
+                make_sex_uniform(process_naziv_file_6(perfume.Naziv, perfume.Tip)["sex"]),
                 process_tip_file_6(perfume.Tip)["volume_tester"],
                 process_tip_file_6(perfume.Tip)["volume_set"],
-                process_naziv_file_6(perfume.Naziv)["volume_amount"],
+                process_naziv_file_6(perfume.Naziv, perfume.Tip)["volume_amount"],
                 process_tip_file_6(perfume.Tip)["volume_metadata"],
                 perfume.Cena,
                 6)
